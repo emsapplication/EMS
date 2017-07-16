3909897d5f6e80bbc448adabfaf9beff2c4937e4
reet <getmscode2013@gmail.com>
2017-07-16 17:20:09 +0530

    account detail Business object

---
+++ EMS.BusinessObjects/AccountDetailBO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EMS.BusinessObjects
+{
+    public class AccountDetailBO : BaseBO
+    {
+
+        public int AccountDetailId { get; set; }
+
+        public string AccountName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int StatusId { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int VerticalId { get; set; }
+
+        public int Director { get; set; }
+
+        public int SDM { get; set; }
+
+        public int PM { get; set; }
+
+       
+    }
+}
---
+++ EMS.BusinessObjects/BaseBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EMS.BusinessObjects
+{
+    public abstract class BaseBO
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public int ModifiedBy { get; set; }
+    }
+}
