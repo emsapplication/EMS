02e4e52cc193714b05a435afbfc7abdc979cb472
Rathore <satyaprakash.rathore@cgi.com>
2017-07-24 22:12:46 +0530

    Remove extra menu

--- EMS/src/app/pages/pages.menu.ts
+++ EMS/src/app/pages/pages.menu.ts
@@ -251,237 +251,237 @@ export const PAGES_MENU = [
         },
       },
 
-      {
-        path: 'charts',
-        data: {
-          menu: {
-            title: 'general.menu.charts',
-            icon: 'ion-stats-bars',
-            selected: false,
-            expanded: false,
-            order: 200,
-          }
-        },
-        children: [
-           {
-            path: 'chartist-js',
-            data: {
-              menu: {
-                subicon: 'fa fa-arrow-circle-o-right',
-                title: 'general.menu.charts',
-              },
-            },
-          },
-        ],
-      },
+      // {
+      //   path: 'charts',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.charts',
+      //       icon: 'ion-stats-bars',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 200,
+      //     }
+      //   },
+      //   children: [
+      //      {
+      //       path: 'chartist-js',
+      //       data: {
+      //         menu: {
+      //           subicon: 'fa fa-arrow-circle-o-right',
+      //           title: 'general.menu.charts',
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
       
-      {
-        path: 'forms',
-        data: {
-          menu: {
-            title: 'general.menu.form_elements',
-            icon: 'ion-compose',
-            selected: false,
-            expanded: false,
-            order: 400,
-          }
-        },
-        children: [
-          {
-            path: 'inputs',
-            data: {
-              menu: {
-                title: 'general.menu.form_inputs',
-              }
-            }
-          },
-          {
-            path: 'layouts',
-            data: {
-              menu: {
-                title: 'general.menu.form_layouts',
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: 'tables',
-        data: {
-          menu: {
-            title: 'general.menu.tables',
-            icon: 'ion-grid',
-            selected: false,
-            expanded: false,
-            order: 500,
-          }
-        },
-        children: [
-          {
-            path: 'basictables',
-            data: {
-              menu: {
-                title: 'general.menu.basic_tables',
-              }
-            }
-          },
-          {
-            path: 'smarttables',
-            data: {
-              menu: {
-                title: 'general.menu.smart_tables',
-              }
-            }
-          },
-          {
-            path: 'datatables',
-            data: {
-              menu: {
-                title: 'Data Tables',
-              }
-            }
-          },
-           {
-             path: 'hottables',
-             data: {
-               menu: {
-                 title: 'Hot Tables',
-               }
-             }
-           }
-        ]
-      },
-      {
-        path: 'maps',
-        data: {
-          menu: {
-            title: 'general.menu.maps',
-            icon: 'ion-ios-location-outline',
-            selected: false,
-            expanded: false,
-            order: 600,
-          }
-        },
-        children: [
-          {
-            path: 'googlemaps',
-            data: {
-              menu: {
-                title: 'general.menu.google_maps',
-              }
-            }
-          },
-          {
-            path: 'leafletmaps',
-            data: {
-              menu: {
-                title: 'general.menu.leaflet_maps',
-              }
-            }
-          },
-          {
-            path: 'bubblemaps',
-            data: {
-              menu: {
-                title: 'general.menu.bubble_maps',
-              }
-            }
-          },
-          {
-            path: 'linemaps',
-            data: {
-              menu: {
-                title: 'general.menu.line_maps',
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'general.menu.pages',
-            icon: 'ion-document',
-            selected: false,
-            expanded: false,
-            order: 650,
-          }
-        },
-        children: [
-          {
-            path: ['/login'],
-            data: {
-              menu: {
-                title: 'general.menu.login'
-              }
-            }
-          },
-          {
-            path: ['/register'],
-            data: {
-              menu: {
-                title: 'general.menu.register'
-              }
-            }
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'general.menu.menu_level_1',
-            icon: 'ion-ios-more',
-            selected: false,
-            expanded: false,
-            order: 700,
-          }
-        },
-        children: [
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'general.menu.menu_level_1_1',
-                url: '#'
-              }
-            }
-          },
-          {
-            path: '',
-            data: {
-              menu: {
-                title: 'general.menu.menu_level_1_2',
-                url: '#'
-              }
-            },
-            children: [
-              {
-                path: '',
-                data: {
-                  menu: {
-                    title: 'general.menu.menu_level_1_2_1',
-                    url: '#'
-                  }
-                }
-              }
-            ]
-          }
-        ]
-      },
-      {
-        path: '',
-        data: {
-          menu: {
-            title: 'general.menu.external_link',
-            url: 'http://akveo.com',
-            icon: 'ion-android-exit',
-            order: 800,
-            target: '_blank'
-          }
-        }
-      }
+      // {
+      //   path: 'forms',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.form_elements',
+      //       icon: 'ion-compose',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 400,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: 'inputs',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.form_inputs',
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: 'layouts',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.form_layouts',
+      //         }
+      //       }
+      //     }
+      //   ]
+      // },
+      // {
+      //   path: 'tables',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.tables',
+      //       icon: 'ion-grid',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 500,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: 'basictables',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.basic_tables',
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: 'smarttables',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.smart_tables',
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: 'datatables',
+      //       data: {
+      //         menu: {
+      //           title: 'Data Tables',
+      //         }
+      //       }
+      //     },
+      //      {
+      //        path: 'hottables',
+      //        data: {
+      //          menu: {
+      //            title: 'Hot Tables',
+      //          }
+      //        }
+      //      }
+      //   ]
+      // },
+      // {
+      //   path: 'maps',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.maps',
+      //       icon: 'ion-ios-location-outline',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 600,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: 'googlemaps',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.google_maps',
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: 'leafletmaps',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.leaflet_maps',
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: 'bubblemaps',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.bubble_maps',
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: 'linemaps',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.line_maps',
+      //         }
+      //       }
+      //     }
+      //   ]
+      // },
+      // {
+      //   path: '',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.pages',
+      //       icon: 'ion-document',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 650,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: ['/login'],
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.login'
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: ['/register'],
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.register'
+      //         }
+      //       }
+      //     }
+      //   ]
+      // },
+      // {
+      //   path: '',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.menu_level_1',
+      //       icon: 'ion-ios-more',
+      //       selected: false,
+      //       expanded: false,
+      //       order: 700,
+      //     }
+      //   },
+      //   children: [
+      //     {
+      //       path: '',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.menu_level_1_1',
+      //           url: '#'
+      //         }
+      //       }
+      //     },
+      //     {
+      //       path: '',
+      //       data: {
+      //         menu: {
+      //           title: 'general.menu.menu_level_1_2',
+      //           url: '#'
+      //         }
+      //       },
+      //       children: [
+      //         {
+      //           path: '',
+      //           data: {
+      //             menu: {
+      //               title: 'general.menu.menu_level_1_2_1',
+      //               url: '#'
+      //             }
+      //           }
+      //         }
+      //       ]
+      //     }
+      //   ]
+      // },
+      // {
+      //   path: '',
+      //   data: {
+      //     menu: {
+      //       title: 'general.menu.external_link',
+      //       url: 'http://akveo.com',
+      //       icon: 'ion-android-exit',
+      //       order: 800,
+      //       target: '_blank'
+      //     }
+      //   }
+      // }
     ]
   }
 ];
